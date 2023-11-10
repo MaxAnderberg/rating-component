0c9b5755716d7cb6ff4909476b9ee045a9b9ee1b
Max <maxanderberg@gmail.com>
2023-11-10 22:39:48 +0100

    added icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <article class="container">
-    <div class="icon">X</div>
+    <div class="icon">
+        <img src="/icon-star.svg" alt="Icon">
+    </div>
     <h1>How did we do?</h1>
     <p>
       Please let us know how we did with your support request.
